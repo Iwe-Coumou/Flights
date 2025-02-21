8e73d259d2e74b6577ed1ce90b55bf8c64895864
iwe-coumou <iweynys@live.nl>
2025-02-21 17:53:04 +0100

    changed main.py for testing

--- scripts/main.py
+++ scripts/main.py
@@ -9,27 +9,27 @@ def main():
     conn = sql.connect("data/flights_database.db")
     month, day = 5, 23,
     
-    # for NYC_airport in NYC_AIRPORTS:
-    # # Plot flight destinations from a specific airport on a given date
-    #     fig, missing_airports = plot_destinations_on_day_from_NYC_airport(conn, month, day, NYC_airport)
-    #     if fig:
-    #         fig.show()
+    for NYC_airport in NYC_AIRPORTS:
+    # Plot flight destinations from a specific airport on a given date
+        fig, missing_airports = plot_destinations_on_day_from_NYC_airport(conn, month, day, NYC_airport)
+        if fig:
+            fig.show()
 
-    # # Plot both airports with and without flights
-    # fig = plot_airports_with_and_without_flights(conn)
-    # if fig:
-    #     fig.show()
+    # Plot both airports with and without flights
+    fig = plot_airports_with_and_without_flights(conn)
+    if fig:
+        fig.show()
 
-    # destination = "ATL"
-    # top_5 = top_5_manufacturers(conn, destination)
-    # print(top_5)
+    destination = "ATL"
+    top_5 = top_5_manufacturers(conn, destination)
+    print(top_5)
 
  
-    # distance_vs_arr_fig, correlation = plot_distance_vs_arr_delay(conn)   
-    # if distance_vs_arr_fig:
-    #     distance_vs_arr_fig.show()
+    distance_vs_arr_fig, correlation = plot_distance_vs_arr_delay(conn)   
+    if distance_vs_arr_fig:
+        distance_vs_arr_fig.show()
 
-    # print(f"Correlation coefficient: {correlation:.3f}")
+    print(f"Correlation coefficient between distance and arrival time delay: {correlation:.3f}")
 
     wind_df = create_flight_dataframe(conn)  # Get flights with precomputed directions
 
