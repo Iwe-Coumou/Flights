bf16279aaf7b8bf2616d3b40790a80a84a337826
iwe-coumou <iweynys@live.nl>
2025-03-05 14:06:10 +0100

    stuff

--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -166,6 +166,22 @@ def add_canceled_column(conn):
     conn.commit()
     print("Canceled flights identified and marked.")
 
+def convert_flight_times(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Converts scheduled and actual arrival/departure times to datetime format.
+    
+    Parameters:
+        flights (pd.DataFrame): The DataFrame containing flight data.
+        
+    Returns:
+        pd.DataFrame: The DataFrame with updated datetime columns.
+    """
+    datetime_columns = ['sched_dep_time', 'dep_time', 'sched_arr_time', 'arr_time']
+
+    df[datetime_columns] = df[datetime_columns].apply(pd.to_datetime)
+
+    return df
+
 def estimate_arr_delay_and_air_time(conn):
     """Estimates missing arr_delay and air_time for flights that have arr_time but missing these values,
     accounting for flights that cross midnight."""
