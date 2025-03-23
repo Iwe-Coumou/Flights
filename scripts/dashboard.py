5e465b13eb2ef334bcaf4f3a82a7cf5ba4f6bd45
iwe-coumou <iweynys@live.nl>
2025-03-23 16:03:30 +0100

    path change

--- scripts/dashboard.py
+++ scripts/dashboard.py
@@ -53,7 +53,7 @@ def get_connection():
     return conn
 
 if 'conn' not in st.session_state:
-    st.session_state.conn = get_connection()
+    st.session_state.conn = sqlite3.connect("/mount/src/flights/data/flights_database.db")
 
 conn = st.session_state.conn
 # ----------------- SIDEBAR STYLING -----------------
